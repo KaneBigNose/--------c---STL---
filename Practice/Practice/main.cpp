711480e11986a9eedefde5750a8205d2a9babfb0
KaneBigNose <imddongs@naver.com>
2024-07-12 21:12:04 +0900

    nothing

--- Practice/Practice/main.cpp
+++ Practice/Practice/main.cpp
@@ -1,69 +1,82 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-template <class T>
-class Queue
+class Point
 {
 private:
-	T* temp;
-	int count;
+	int x, y, r;
 public:
-	explicit Queue() : temp(NULL), count(0) {}
-	~Queue()
+	Point(int x = 0, int y = 0, int r = 0) : x(x), y(y), r(r) {}
+	bool operator==(Point p)
 	{
-		delete[]temp;
-	}
-	void Push(T num)
-	{
-		if (temp == NULL)
+		if (x == p.x && y == p.y)
 		{
-			temp = new T[10];
-			temp[count++] = num;
+			return true;
 		}
 		else
 		{
-			temp[count++] = num;
+			return false;
 		}
 	}
-	bool Empty()
+	bool operator!=(Point p)
 	{
-		return temp == NULL;
-	}
-	T Pop()
-	{
-		T thistemp = temp[0];
-		for (int i = 0; i <= count; i++)
+		if (x == p.x && y == p.y)
 		{
-			temp[i] = temp[i + 1];
+			return false;
 		}
-		temp[count--] = NULL;
-		return thistemp;
-	}
-	void Print()
-	{
-		for (int i = 0; i < count; i++)
+		else
 		{
-			cout << temp[i];
+			return true;
 		}
+	}
+	int distance(Point p) const
+	{
+		return pow(abs(x - p.x), 2) + pow(abs(y - p.y), 2); // 거리의 제곱
 	}
 };
 
-int main(void)
+int main()
 {
-	Queue<int> q;
-
-	q.Push(10);
-	q.Push(20);
-	q.Push(30);
-
-	if (!q.Empty())
-		cout << q.Pop() << endl;
-	if (!q.Empty())
-		cout << q.Pop() << endl;
-	if (!q.Empty())
-		cout << q.Pop() << endl;
-
-	q.Print();
-
-	return 0;
+	int test;
+	cin >> test;
+	int x1, y1, r1, x2, y2, r2;
+	for (int i = 0; i < test; i++)
+	{
+		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+		Point p1(x1, y1, r1), p2(x2, y2, r2);
+		int powR = pow(r1 + r2, 2);
+		if (p1 != p2 && p1.distance(p2) < powR) // 접점이 2개인 경우
+		{
+			cout << "2" << endl;
+		}
+		else if (p1 != p2 && p1.distance(p2) == powR) // 접점이 1개인 경우 (외접)
+		{
+			cout << "1" << endl;
+		}
+		else if (p1 != p2 && pow(r1, 2) == p1.distance(p2) + pow(r2, 2)) // 접점이 1개인 경우 (내접)
+		{
+			cout << "1" << endl;
+		}
+		else if (p1 != p2 && p1.distance(p2) > powR) // 접점이 0개인 경우 (외부)
+		{
+			cout << "0" << endl;
+		}
+		else if (p1 != p2 && pow(r1, 2) > p1.distance(p2) + pow(r2, 2)) // 접점이 0개인 경우 (내부, 중심이 다른 경우)
+		{
+			cout << "0" << endl;
+		}
+		else if (p1 == p2 && r1 != r2) // 접점이 0개인 경우 (내부, 중심이 같은 경우)
+		{
+			cout << "0" << endl;
+		}
+		else if (p1 == p2 && r1 == r2) // 접점이 무한인 경우
+		{
+			cout << "-1" << endl;
+		}
+		else
+		{
+			cout << "예외 발생!" << endl;
+		}
+	}
 }
